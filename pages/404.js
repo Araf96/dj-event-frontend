f18256554bd6875f982643684f19ed834fab41eb
Araf96 <rafatspn@gmail.com>
2022-11-21 16:21:10 +0600

    custom 404 error added

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import Layout from "../Components/Layout"
+import Link from "next/link"
+import {FaExclamationTriangle} from 'react-icons/fa'
+
+import styles from "../styles/404.module.css"
+
+export default function PageNotFoundPage() {
+  return (
+    <Layout>
+      <div className={styles.error}>
+        <h1><FaExclamationTriangle/> 404</h1>
+        <h4>Sorry, nothing in here</h4>
+        <Link href="/">Go back to home</Link>
+      </div>
+    </Layout>
+  );
+}
